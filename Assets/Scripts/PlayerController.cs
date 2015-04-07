da8d433ead6eecb71bf23456602e1ea1dfc1310a
Luiz Strobelt <lhsperez@gmail.com>
2015-04-07 13:18:30 -0300

    Score basico

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,13 @@ public class PlayerController : MonoBehaviour {
 
 	public float jumpHeight;
 	public Camera mainCamera;
+	public GameObject floor;
 	public GUIText centerText;
+	public GUIText scoreText;
 
 	private Rigidbody rBody;
+	private float maxDistance = 0.0f;
+	private bool gameIsOver = false;
 
 	void Start()
 	{
@@ -23,24 +27,43 @@ public class PlayerController : MonoBehaviour {
 
 	void Update()
 	{
-		rBody.position = new Vector3 (
+		if (!gameIsOver) 
+		{
+			// Move o jogador na horizontal
+			rBody.position = new Vector3 (
 			Mathf.Clamp (rBody.position.x, -4.2f, 4.2f), 
 			rBody.position.y, 
 			rBody.position.z
 			);
 
-		if (!CanIBeSeen ()) 
-		{
-			centerText.text = "Perdeu :(";
-		}
+			// Verifica se o jogador saiu de vista
+			if (!CanIBeSeen ()) {
+				GameOver ();
+			}
+
+			// Atualiza pontuaçao
+			float distance = Vector3.Distance (GetComponent<Transform> ().position, floor.transform.position);
+			if (distance > maxDistance)
+				maxDistance = distance;
+			scoreText.text = Mathf.FloorToInt (maxDistance * 100) + " pts.";
+		}	
+	}
+
+	private void GameOver()
+	{
+		centerText.text = "Game Over :(\n" + scoreText.text;
+		scoreText.text = "";
+		gameIsOver = true;
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
-		bool isUnderPlayer = other.gameObject.transform.position.y < rBody.position.y;
-		if (other.gameObject.tag.Equals("Platform") && isUnderPlayer) 
+		if (!gameIsOver) 
 		{
-			Jump();
+			bool isUnderPlayer = other.gameObject.transform.position.y < rBody.position.y;
+			if (other.gameObject.tag.Equals ("Platform") && isUnderPlayer) {
+				Jump ();
+			}
 		}
 	}
 
